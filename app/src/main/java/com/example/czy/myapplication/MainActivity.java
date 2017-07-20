afd4f1231c041f4170929e71c5dda9a9be168a36
czy <2455950287@qq.com>
2017-07-20 09:18:18 +0800

    4

--- app/src/main/java/com/example/czy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/czy/myapplication/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.czy.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.mylibrary.RequestBody.RequsetParams;
+import com.example.mylibrary.base.BaseActivity;
+import com.example.mylibrary.base.ResultBase;
+import com.example.mylibrary.prsenter.TestPresenter;
+import com.example.mylibrary.view.TestView;
+
+import io.reactivex.Observable;
+
+public class MainActivity extends BaseActivity implements TestView {
+    private TestPresenter testPresenter = new TestPresenter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testPresenter.attachView(this);
+        testPresenter.getData(app.getApiService().getTest(new RequsetParams()));
+    }
+
+    @Override
+    public void getData(String data) {
+
+    }
+
+    @Override
+    public void getDataErr(String err) {
 
     }
 }
